f1ceb8e1621c1de24c2de7c3ab5250c5ba462bdd
Rafael Furry <rfurry@gmail.com>
2017-09-15 10:05:14 -0700

    create word list

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -6,5 +6,12 @@ describe 'Word' do
         it "starts empty" do
             expect(Word.list()).to(eq([]))
         end
+        it "creates list of words" do
+            word1 = Word.new("foo")
+            word1.save
+            word2 = Word.new("bar")
+            word2.save
+            expect(Word.list()).to(eq([word1, word2]))
+        end
     end
 end
